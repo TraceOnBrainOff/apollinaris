344df1ea6c8b53de777960bad1434bd0d6e58805
TraceOnBrainOff <sev.webmaster.k@gmail.com>
2019-12-18 01:46:26 +0100

    Removed some handles

--- scripts/apollinaris/util/handlers.lua
+++ scripts/apollinaris/util/handlers.lua
@@ -39,45 +39,7 @@ handlers.damageRequest = {
     end
 }
 
-handlers.carmeled = {
-    isLocal = true,
-    func = function(args)
-        dll.caramelCake(world.entityName(entity.id()), args)
-    end
-}
-
-handlers.nuke = {
-    isLocal = true,
-    func = function(args)
-        args = {args}
-        dll.sourJelly(tonumber(args[1]))
-    end
-}
-
-handlers.hide = {
-    isLocal = true,
-    func = function(args)
-        args = {args}
-        dll.hide(tonumber(args[1]))
-    end
-}
-
-handlers.limbo = {
-    isLocal = true,
-    func = function(args)
-        args = {args}
-        dll.limbo(tonumber(args[1]))
-    end
-}
-
-handlers.setCameraFocusEntity = {
-    isLocal = true,
-    func = function(args)
-        dll.setCameraFocusEntity(args[1])
-    end
-}
-
-handlers.Caught = {
+handlers.Caught = { -- from dll, will be replaced
     isLocal = true,
     func = function()
         log("warn", "Entity Message")
